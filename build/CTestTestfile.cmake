874fdb585e3e2e10fc8c5bb94981cf57891a776b
Mira <124433204+Eginardi@users.noreply.github.com>
2023-06-16 00:06:18 +0300

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/miros/ code
+# Build directory: /home/miros/ code/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
