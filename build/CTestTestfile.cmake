eb943db8f9d7e3ce42403beae6e5bd6e4b982fb7
Mira <124433204+Eginardi@users.noreply.github.com>
2023-06-16 00:10:21 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miros/ code
-# Build directory: /home/miros/ code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
